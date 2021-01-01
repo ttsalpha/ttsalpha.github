03b55000f6d00b627584b85006dea115d9b9c5a9
ttsalpha <ttsalpha@icloud.com>
2021-01-02 00:21:23 +0700

    update scripts to send a message

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,14 @@
  * 3. Menu Mobile
  * 4. Project Detail
  * 5. Preload
+ * 6. Fill GG Form
  */
 
 'use strict';
 
-(function($) {
+(function ($) {
 
-    $.fn.kellyMagnificPopup = function(opts) {
+    $.fn.kellyMagnificPopup = function (opts) {
 
         var $self = $(this),
             options = $.extend({
@@ -33,23 +34,23 @@
                 },
                 closeMarkup: '<button title="%title%" type="button" class="mfp-close"></button>',
                 callbacks: {
-                    markupParse: function(item) {},
-                    imageLoadComplete: function() {
+                    markupParse: function (item) { },
+                    imageLoadComplete: function () {
                         var $container = $('.mfp-container');
 
                         $container.addClass('load-done');
-                        setTimeout(function() {
+                        setTimeout(function () {
                             $container.addClass('load-transition');
                         }, 50);
                     },
-                    change: function() {
+                    change: function () {
                         var $container = $('.mfp-container');
                         $container.removeClass('load-done load-transition');
                     }
 
                 }
             }, $self.data(), opts);
-        $('.media', $self).each(function() {
+        $('.media', $self).each(function () {
             var href = $(this).data('url');
 
             if (href && href !== '') {
@@ -60,7 +61,7 @@
         $self.magnificPopup(options);
     };
 
-    $(document).ready(function() {
+    $(document).ready(function () {
 
         var $header = $('.header'),
             $search = $('.fa-search', $header);
@@ -69,18 +70,18 @@
         if ($search.length) {
             var $boxSearch = $('.box-search', $header);
 
-            $search.on('click', function(event) {
+            $search.on('click', function (event) {
                 event.preventDefault();
                 $boxSearch.addClass('active');
                 $('.search-field', $boxSearch).focus();
             });
 
-            $('.kd-close', $boxSearch).on('click', function(event) {
+            $('.kd-close', $boxSearch).on('click', function (event) {
                 event.preventDefault();
                 $boxSearch.removeClass('active');
             });
 
-            $(document).on('keydown', function(event) {
+            $(document).on('keydown', function (event) {
 
                 if (event.keyCode === 27) {
 
@@ -112,7 +113,7 @@
         var $btnMenu = $('.menu-mobile'),
             $hideMenu = $('.hide-menu');
 
-        $btnMenu.on('click', function() {
+        $btnMenu.on('click', function () {
             $header.toggleClass('active');
 
             if ($header.hasClass('active')) {
@@ -121,12 +122,12 @@
                 $hideMenu.removeClass('active');
             }
         });
-        $hideMenu.on('click', function() {
+        $hideMenu.on('click', function () {
             $header.removeClass('active');
             $hideMenu.removeClass('active');
         });
 
-        $('.menu-item-has-children', '.main-menu').on('click', ' > a', function(e) {
+        $('.menu-item-has-children', '.main-menu').on('click', ' > a', function (e) {
             var ww = $(window).width();
 
             if (ww <= 991) {
@@ -151,10 +152,54 @@
         var $preload = $('#preload');
 
         if ($preload.length) {
-            $(window).on('load', function() {
+            $(window).on('load', function () {
                 $preload.fadeOut(400);
             });
         }
+    });
+
+    // 6. Fill GG Form
+
+    // Process contact form
+    $('#contact-form').submit(function (event) {
+        event.preventDefault();
+        $('#feedback').html('');
+        setTimeout(function () {
+            // Get data
+            var data = {
+                'entry.2005620554': $('#form-name').val(),
+                'entry.1045781291': $('#form-email').val(),
+                'entry.839337160': $('#form-message').val()
+            };
+
+            // Validate form
+            var formSuccess = true;
+            Object.keys(data).forEach(function (key, index) {
+                if (!data[key]) {
+                    formSuccess = false;
+                    $('#feedback').html('<label class="text-danger">Please complete all fields</label>');
+                }
+            });
+
+            if (formSuccess) {
+                // Send request
+                $.ajax({
+                    url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScOF3Q0pAZ4PwGva37az1S4j0ssAOSqs1VPUotmTKgxTc_W_Q/formResponse',
+                    type: 'POST',
+                    crossDomain: true,
+                    dataType: "xml",
+                    data: data,
+                    success: function (jqXHR, textStatus, errorThrown) {
+                        console.log('Enter on success');
+                        $('#feedback').html('<label class="text-success">Message sent!</label>');
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        console.log('Enter on error');
+                        $('#feedback').html('<label class="text-success">Message sent!</label>');
+                    }
+                });
+            }
+        }, 300);
     });
 
 })(jQuery);
